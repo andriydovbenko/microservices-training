715be1a3b24b6dbe8fedcc1781c5b2d477fcacd3
andriydovbenko <andriydovbenko1@gmail.com>
2020-03-16 15:43:37 +0100

    created clien server movie-catalog-server

---
+++ movie-catalog-service/src/main/java/com/cursor/moviecatalogservice/domain/Movie.java
@@ -0,0 +1,15 @@
+package com.cursor.moviecatalogservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    private String id;
+    private String title;
+    private String description;
+}
